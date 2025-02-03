75405b15ae04b0aed46c150c218bd66704803ee2
nedim.k <nedim.k@cornertable.app>
2025-02-03 17:00:32 +0100

    landing page

--- Landing-Page/app/routes/landingPage.tsx
+++ Landing-Page/app/routes/landingPage.tsx
@@ -1,74 +1,82 @@
 import {Link} from "@remix-run/react";
 
 export default function LandingPage() {
+
   const texts = [
     "Find your favorite bar's favorite bars.",
     "Enjoy your favorite restaurant's favorite restaurant.",
     "Meet your favorite bartender's favorite bartender.",
     "Visit your favorite chef's favorite chefs."
   ];
+
   const randomText = texts[Math.floor(Math.random() * texts.length)];
   return (
-    <div className="items-center flex flex-col md:w-[25rem] md:h-[64rem] lg:w-[1440px]">
-      <div className="flex ite-center sm:text-left sm:w-full sm:h-[4.5rem] sm:px-[1.75rem] sm:py-[1rem] sm:gap-[1.75rem]
-      sm:border-b-white-[0.05rem] justify-between">
+    <div className="items-center flex flex-col w-[25rem] h-[64rem] overflow-hidden lg:w-[90rem]">
+      <div className="flex items-center text-left w-fit h-[4.5rem] px-[1.75rem] py-[1rem] gap-[1.75rem]
+      border-b-white-[0.05rem] justify-between lg:h-[4rem] lg:w-[70rem]">
         <img
           src="/assets/Corner_Table_LogoMay20242.png"
           alt="Corner Table Logo"
-          className="sm:w-[6.25rem] sm:h-[2.5rem]"
+          className="w-[6.25rem] h-[2.5rem]"
         />
-        <p className="font-custom text-textColor font-normal text-[0.75rem]">
-          Do you work in the hospitality industry?{" "}
-          <Link to="hospitality" className="text-customBrown">
+        <div className={'flex flex-none items-center w-auto gap-1'}>
+          <p className="font-custom text-textColor text-right font-normal text-[0.75rem] hidden lg:block">
+            Do you in the hospitality industry?{" "}
+          </p>
+          <p className="lg:hidden font-custom text-textColor text-right font-normal text-[0.75rem]">
+            Are you in the industry?{" "}
+          </p>
+          <Link to="hospitality" className={"font-custom text-[0.75rem] text-customBrown"}>
             Go here
           </Link>
-        </p>
-      </div>
-      <div className="flex flex-col sm:w-[25rem] sm:h-[18.4rem] sm:p-[1rem] sm:gap-[1.75rem]">
-        <div className="flex-col sm:w-full sm:h-auto sm:gap-4">
-          <div className={"sm:w-full sm:gap-4 sm:h-auto"}>
-          <p className="font-custom font-light sm:text-[2rem] text-center sm:italic text-textTertiaryColor">
-            Be a regular{" "}<br/>
-            <span className="font-semibold text-primaryColor">
-            anywhere in the world
-          </span>
-          </p>
-          <p className="sm:w-[22rem] sm:h-[4rem] font-custom text-[0.8rem] font-normal text-secondary sm:leading-[1.3rem] text-center">
-            {randomText}
-          </p>
-          </div>
-          <div className="flex flex-col h-auto w-[23rem] sm:gap-4">
-            <input
-              type="email"
-              placeholder="Enter your email address"
-              className="sm:w-full sm:h-[3.25rem] sm:rounded-[0.25rem] bg-inputColor sm:py-5 sm:gap-2.5"
-            />
-            <button className="sm:w-full sm:h-auto sm:rounded sm:px-3 sm:py-4 sm:gap-1 bg-customBrown text-white">
-              Get notified
-            </button>
-          </div>
         </div>
       </div>
-      <div className="w-full lg:w-auto flex justify-center">
+      <div className={"lg:flex lg:flex-row lg:gap-[3rem] items-center lg:h-[48rem] lg:w-[70rem] lg:items-center"}>
+        <div className="flex flex-col w-[25rem] h-[18.4rem] p-[1rem] gap-[1.75rem] lg:w-[30rem] lg:h-[18rem] lg:text-left">
+          <div className="flex-col w-full h-auto gap-4 lg:flex lg:flex-col lg:items-center lg:gap-5">
+            <div className={"w-full gap-4 h-auto"}>
+              <p className="font-custom font-light text-[2rem] text-center italic lg:not-italic text-textTertiaryColor">
+                Be a regular{" "}<br/>
+                <span className="not-italic font-semibold text-primaryColor">
+              anywhere in the world
+              </span>
+              </p>
+              <p
+                className="w-[22rem] h-[4rem] font-custom text-[0.8rem] font-normal text-secondary leading-[1.3rem] text-center">
+                {randomText}
+              </p>
+            </div>
+            <div className="flex flex-col h-auto w-[23rem] gap-4">
+              <input
+                type="email"
+                placeholder="Enter your email address"
+                className="font-custom text-[0.8rem] w-full h-[3.25rem] rounded-[0.25rem] bg-inputColor py-5 pl-[1rem] gap-2.5"
+              />
+              <button className="w-full h-auto rounded px-3 py-4 gap-1 bg-customBrown text-white">
+                Get notified
+              </button>
+            </div>
+          </div>
+        </div>
         <img
-          className="w-[26rem]"
+          className="relative object-fill w-[26rem] h-[36rem] rotate--180 left-[3.5rem]"
           src="assets/Version01.png"
           alt="MobileView"
         />
       </div>
-      <div
-        className="w-[80rem] h-10 pt-6 mx-10 border-t border-borderColor flex justify-between ite-center
-        text-paragraphColor">
-        <div className="font-custom text-sm">
-          © 2024 Corner Table App
-        </div>
-        <div className="flex gap-4 font-custom text-sm">
-          <Link to="/contact">
+      <div className="flex flex-col w-full h-auto py-5 gap-1.5 border-t border-borderColor items-center
+        text-secondary">
+        <div className="h-auto gap-1 flex items-center">
+          <Link to="#">
             Contact us
           </Link>
-          <Link to="/privacy-policy">
+          <img className={"flex w-[3px] h-[4px]  "} src={"assets/Ellipse.png"} alt="Dot"/>
+          <Link to={'#'}>
             Privacy policy
           </Link>
+        </div>
+        <div className="font-custom text-sm">
+          © 2024 Corner Table App
         </div>
       </div>
     </div>
