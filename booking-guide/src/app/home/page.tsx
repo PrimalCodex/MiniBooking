7f3c5485a8db75555c453b3b49a21ade10cdda8c
PrimalCodex <nkevro31@gmial.com>
2025-06-09 19:53:41 +0200

    Header init

--- booking-guide/src/app/home/page.tsx
+++ booking-guide/src/app/home/page.tsx
@@ -22,7 +22,7 @@ export default function HomePage() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <h1 className="text-3xl font-bold text-blue-700">
-        Welcome {user?.email || "Guest"} 🏡
+        Welcdadadasdadasdasdasdasdasdadadome {user?.email || "Guest"} 🏡
       </h1>
     </div>
   );
